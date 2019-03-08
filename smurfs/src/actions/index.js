0d49942a4ce67703536dcd71ec136953c76be1de
Dan Sherman <dvsherman@protonmail.com>
2019-03-08 09:52:20 -0700

    created getSmurfs action, added additionally needed component files

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,18 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
+
+const url = 'http://localhost:3333/smurfs';
+
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAIL = 'FETCH_SMURF_FAIL';
+
+export const getSmurfs = () => dispatch => {
+  dispatch ({ type: FETCH_SMURF_START });
+  axios
+    .get(url)
+    .then(res => dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: FETCH_SMURF_FAIL, payload: err }));
+};
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
