650be50283660190b6955ef873152368c5f61126
Dan Sherman <dvsherman@protonmail.com>
2019-03-08 10:24:05 -0700

    sidetracked to update server, working on displaying smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { fetchingSmurfs } from '../actions'
+import Smurf from './Smurf';
+// import SmurfForm from '../components/SmurfForm';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,15 +11,29 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchingSmurfs();
+  };
+
   render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <SmurfForm />
-        <Smurfs />
-      </div>
-    );
+      return (
+          <div>
+              {this.props.smurfs.map((smurf, index) => (
+                  <Smurf
+                  key={index}
+                  smurf={smurf}
+                  />
+              ))}
+          </div>
+      )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+smurfs: state.smurfs
+});
+
+export default connect(
+mapStateToProps,
+{ fetchingSmurfs }
+)(App);
---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Smurf = props => {
+    return (
+        <div className="smurf">
+            <h3>{props.}</h3>
+        </div>
+    )
+}
